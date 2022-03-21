abaad713dc186b0dbc0988efc30a152d51400a9c
cmrib <ciceromirandar@gmail.com>
2022-03-21 08:38:02 -0300

    add feather icons

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import { GetStaticProps } from 'next';
 
 import { getPrismicClient } from '../services/prismic';
+import { FiUser, FiCalendar } from 'react-icons/fi'
 
 import commonStyles from '../styles/common.module.scss';
 import styles from './home.module.scss';
@@ -26,15 +27,69 @@ interface HomeProps {
 
 export default function Home() {
 
+
   return (
-    <h1>Cicero</h1>
+    <>
+
+      <div className={styles.container}>
+
+        <img src="./logo.png" alt="logo" />
+        <div>
+          <h3>Como utilizar Hooks</h3>
+          <p>Pensando em sincronização em vez de ciclos de vida.</p>
+          <span> <FiCalendar /> 21 Mar 2021</span>
+          <span> <FiUser /> Cicero Ribeiro</span>
+        </div>
+
+        <br />
+
+        <div>
+          <h3>Como utilizar Hooks</h3>
+          <p>Pensando em sincronização em vez de ciclos de vida.</p>
+          <span> <FiCalendar /> 21 Mar 2021</span>
+          <span> <FiUser /> Cicero Ribeiro</span>
+        </div>
+
+        <br />
+
+        <div>
+          <h3>Como utilizar Hooks</h3>
+          <p>Pensando em sincronização em vez de ciclos de vida.</p>
+          <span> <FiCalendar /> 21 Mar 2021</span>
+          <span> <FiUser /> Cicero Ribeiro</span>
+        </div>
+
+        <br />
+
+        <div>
+          <h3>Como utilizar Hooks</h3>
+          <p>Pensando em sincronização em vez de ciclos de vida.</p>
+          <span> <FiCalendar /> 21 Mar 2021</span>
+          <span> <FiUser /> Cicero Ribeiro</span>
+        </div>
+
+        <button className={styles.loadButton}>
+          Carregar mais posts
+        </button>
+
+      </div>
+    </>
   )
 
 }
 
-// export const getStaticProps = async () => {
-//   // const prismic = getPrismicClient();
-//   // const postsResponse = await prismic.query(TODO);
+export const getStaticProps = async () => {
+  // const prismic = getPrismicClient();
+  // const postsResponse = await prismic.query(TODO);
 
-//   // TODO
-// };
+  // TODO
+
+  const data = {
+    name: 'Cicero',
+    age: 27
+  }
+
+  return {
+    props: { data }
+  }
+};
